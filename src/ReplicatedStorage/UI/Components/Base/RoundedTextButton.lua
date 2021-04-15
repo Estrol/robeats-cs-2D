b8e05134709eb8523c40114f66db925b4f3bbf66
thenerdie <stirmanjr@gmail.coom>
2021-04-15 16:25:56 -0400

    More patches

--- src/ReplicatedStorage/UI/Components/Base/RoundedTextButton.lua
+++ src/ReplicatedStorage/UI/Components/Base/RoundedTextButton.lua
@@ -12,8 +12,10 @@ RoundedTextButton.defaultProps = {
     Text = "RoundedTextButton";
     TextSize = 18;
     BackgroundColor3 = Color3.fromRGB(29, 29, 29);
-    ShrinkBy = 10;
+    ShrinkBy = 0.1;
     HighlightBackgroundColor3 = Color3.fromRGB(17, 17, 17);
+    OnPress = noop;
+    OnRelease = noop
 }
 
 function RoundedTextButton:init()
@@ -49,9 +51,19 @@ end
 
 function RoundedTextButton:render()
     local props = {
+        TextColor3 = self.props.TextColor3;
+        Font = self.props.Font;
+        Text = self.props.Text;
+        TextSize = self.props.TextSize;
+        AnchorPoint = self.props.AnchorPoint;
+        Position = self.props.Position;
+        TextScaled = self.props.TextScaled;
+        TextXAlignment = self.props.TextXAlignment;
+        TextYAlignment = self.props.TextYAlignment;
+        AutoButtonColor = false;
         Size = self.motorBinding:map(function(a)
-            local shrinkByX = self.props.suppressXAxis and 0 or self.props.shrinkBy*a.tap
-            local shrinkByY = self.props.suppressYAxis and 0 or self.props.shrinkBy*a.tap
+            local shrinkByX = self.props.suppressXAxis and 0 or self.props.ShrinkBy*a.tap
+            local shrinkByY = self.props.suppressYAxis and 0 or self.props.ShrinkBy*a.tap
             return self.props.Size - UDim2.fromScale(shrinkByX, shrinkByY)
         end);
         BackgroundColor3 = self.motorBinding:map(function(a)
@@ -74,6 +86,7 @@ function RoundedTextButton:render()
             })
         end;
         [Roact.Event.MouseButton1Down] = function()
+            self.props.OnPress()
             self.motor:setGoal({
                 tap = Flipper.Spring.new(0.9, {
                     frequency = 8;
@@ -82,6 +95,7 @@ function RoundedTextButton:render()
             })
         end;
         [Roact.Event.MouseButton1Up] = function()
+            self.props.OnRelease()
             self.motor:setGoal({
                 tap = Flipper.Spring.new(0.7, {
                     frequency = 8;
@@ -89,13 +103,8 @@ function RoundedTextButton:render()
                 })
             })
         end;
+        [Roact.Event.MouseButton1Click] = self.props.OnClick;
     }
-
-    for i, v in pairs(self.props) do
-        if props[i] ==  nil then
-            props[i] = v
-        end
-    end
 
     return Roact.createElement("TextButton", props, {
         Corner = Roact.createElement("UICorner", {
