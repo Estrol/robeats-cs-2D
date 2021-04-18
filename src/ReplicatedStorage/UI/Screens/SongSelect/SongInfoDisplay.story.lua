1a4100c9d77f400ef482dcfcca8fc65171d0ed78
thenerdie <stirmanjr@gmail.coom>
2021-04-17 23:46:46 -0400

    Fix SongKey prop

--- src/ReplicatedStorage/UI/Screens/SongSelect/SongInfoDisplay.story.lua
+++ src/ReplicatedStorage/UI/Screens/SongSelect/SongInfoDisplay.story.lua
@@ -3,7 +3,7 @@ local SongInfoDisplay = require(game.ReplicatedStorage.UI.Screens.SongSelect.Son
 
 return function(target)
     local app = Roact.createElement(SongInfoDisplay, {
-        SongKey = "9c91cb040aa9779af4cc3fb291e0eb72"
+        SongKey = 1
     })
     local handle = Roact.mount(app, target)
 
