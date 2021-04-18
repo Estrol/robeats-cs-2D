f2c37f717c380929febb3db94b059c78b6445ecc
thenerdie <stirmanjr@gmail.coom>
2021-04-17 23:44:04 -0400

    Implement song list for SongSelect

--- src/ReplicatedStorage/UI/Screens/SongSelect/init.lua
+++ src/ReplicatedStorage/UI/Screens/SongSelect/init.lua
@@ -4,12 +4,13 @@ local e = Roact.createElement
 local RoundedFrame = require(game.ReplicatedStorage.UI.Components.Base.RoundedFrame)
 
 local SongInfoDisplay = require(script.SongInfoDisplay)
+local SongList = require(script.SongList)
 
 local SongSelect = Roact.Component:extend("SongSelect")
 
 function SongSelect:init()
     self:setState({
-        selectedSongKey = "9c91cb040aa9779af4cc3fb291e0eb72"
+        selectedSongKey = 1
     })
 end
 
@@ -18,10 +19,20 @@ function SongSelect:render()
 
     }, {
         SongInfoDisplay = e(SongInfoDisplay, {
-            Size = UDim2.fromScale(0.6, 0.2),
+            Size = UDim2.fromScale(0.985, 0.2),
             Position = UDim2.fromScale(0.01, 0.01),
             SongKey = self.state.selectedSongKey
-        })    
+        }),
+        SongList = e(SongList, {
+            Size = UDim2.fromScale(0.45, 0.77),
+            AnchorPoint = Vector2.new(1, 1),
+            Position = UDim2.fromScale(0.995, 0.985),
+            OnSongSelected = function(key)
+                self:setState({
+                    selectedSongKey = key
+                })
+            end
+        })
     })
 end
 
