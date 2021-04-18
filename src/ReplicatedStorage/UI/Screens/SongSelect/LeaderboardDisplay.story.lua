df438b94609dee7743d4e6360fa392118ccbe5f2
thenerdie <stirmanjr@gmail.coom>
2021-04-17 23:45:19 -0400

    Change casing style

--- src/ReplicatedStorage/UI/Screens/SongSelect/LeaderboardDisplay.story.lua
+++ src/ReplicatedStorage/UI/Screens/SongSelect/LeaderboardDisplay.story.lua
@@ -1,89 +1,89 @@
 local Roact = require(game.ReplicatedStorage.Packages.Roact)
+local LeaderboardDisplay = require(game.ReplicatedStorage.UI.Screens.SongSelect.LeaderboardDisplay)
 
-local LeaderboardDisplay = require(game.ReplicatedStorage.Client.Components.Screens.SongSelect.LeaderboardDisplay)
+return function(target)
+    local leaderboard = {
+        {
+            UserId = 526993347,
+            PlayerName = "kisperal",
+            Marvelouses = 6,
+            Perfects = 5,
+            Greats = 4,
+            Goods = 3,
+            Bads = 2,
+            Misses = 1,
+            Time = 1596444113,
+            Accuracy = 98.98,
+            Place = 1,
+            Score = 0
+        },
+        {
+            UserId = 160677253,
+            PlayerName = "DetWasTaken",
+            Marvelouses = 6,
+            Perfects = 5,
+            Greats = 4,
+            Goods = 3,
+            Bads = 2,
+            Misses = 1,
+            Time = 1596666465,
+            Accuracy = 95.67,
+            Place = 2,
+            Score = 0
+        },
+        {
+            UserId = 160677253,
+            PlayerName = "DetWasTaken",
+            Marvelouses = 6,
+            Perfects = 5,
+            Greats = 4,
+            Goods = 3,
+            Bads = 2,
+            Misses = 1,
+            Time = 1596666465,
+            Accuracy = 95.67,
+            Place = 3,
+            Score = 0
+        },
+        {
+            UserId = 160677253,
+            PlayerName = "DetWasTaken",
+            Marvelouses = 6,
+            Perfects = 5,
+            Greats = 4,
+            Goods = 3,
+            Bads = 2,
+            Misses = 1,
+            Time = 1596666465,
+            Accuracy = 95.67,
+            Place = 4,
+            Score = 0
+        },
+        {
+            UserId = 160677253,
+            PlayerName = "DetWasTaken",
+            Marvelouses = 6,
+            Perfects = 5,
+            Greats = 4,
+            Goods = 3,
+            Bads = 2,
+            Misses = 1,
+            Time = 1596666465,
+            Accuracy = 95.67,
+            Place = 5,
+            Score = 0
+        }
+    }
 
-local Story = require(game.ReplicatedStorage.Shared.Story)
-
-local LeaderboardDisplayApp = Story:new()
-
-function LeaderboardDisplayApp:render()
-    return Roact.createElement(LeaderboardDisplay, {
-        leaderboard = {
-            {
-                userid = 526993347,
-                playername = "kisperal",
-                marvelouses = 6,
-                perfects = 5,
-                greats = 4,
-                goods = 3,
-                bads = 2,
-                misses = 1,
-                time = 1596444113,
-                accuracy = 98.98,
-                place = 1,
-                score = 0
-            },
-            {
-                userid = 160677253,
-                playername = "DetWasTaken",
-                marvelouses = 6,
-                perfects = 5,
-                greats = 4,
-                goods = 3,
-                bads = 2,
-                misses = 1,
-                time = 1596666465,
-                accuracy = 95.67,
-                place = 2,
-                score = 0
-            },
-            {
-                userid = 160677253,
-                playername = "DetWasTaken",
-                marvelouses = 6,
-                perfects = 5,
-                greats = 4,
-                goods = 3,
-                bads = 2,
-                misses = 1,
-                time = 1596666465,
-                accuracy = 95.67,
-                place = 3,
-                score = 0
-            },
-            {
-                userid = 160677253,
-                playername = "DetWasTaken",
-                marvelouses = 6,
-                perfects = 5,
-                greats = 4,
-                goods = 3,
-                bads = 2,
-                misses = 1,
-                time = 1596666465,
-                accuracy = 95.67,
-                place = 4,
-                score = 0
-            },
-            {
-                userid = 160677253,
-                playername = "DetWasTaken",
-                marvelouses = 6,
-                perfects = 5,
-                greats = 4,
-                goods = 3,
-                bads = 2,
-                misses = 1,
-                time = 1596666465,
-                accuracy = 95.67,
-                place = 5,
-                score = 0
-            },
-        };
+    local app = Roact.createElement(LeaderboardDisplay, {
+        leaderboard = leaderboard,
         Size = UDim2.new(1, 0, 1, 0);
         Position = UDim2.new(0.5, 0, 0.5, 0),
         AnchorPoint = Vector2.new(0.5, 0.5),
     })
-end
+    local handle = Roact.mount(app, target)
 
-return LeaderboardDisplayApp
+    return function()
+        Roact.unmount(handle)
+    end
+end
