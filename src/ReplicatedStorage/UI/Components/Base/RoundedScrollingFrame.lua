c283369e7a402dd1fbf3dab68d1e4f409681b1e4
thenerdie <stirmanjr@gmail.coom>
2021-04-18 20:39:35 -0400

    Add background to RoundedScrollingFrame

--- src/ReplicatedStorage/UI/Components/Base/RoundedScrollingFrame.lua
+++ src/ReplicatedStorage/UI/Components/Base/RoundedScrollingFrame.lua
@@ -1,16 +1,22 @@
 local Roact = require(game.ReplicatedStorage.Packages.Roact)
+local e = Roact.createElement
+local f = Roact.createFragment
+
 local Llama = require(game.ReplicatedStorage.Packages.Llama)
+
+local RoundedFrame = require(game.ReplicatedStorage.UI.Components.Base.RoundedFrame)
 
 local RoundedScrollingFrame = Roact.Component:extend("Button")
 
 RoundedScrollingFrame.defaultProps = {
     Size = UDim2.fromScale(1, 1),
     BackgroundColor3 = Color3.fromRGB(29, 29, 29);
+    ZIndex = 1
 }
 
 function RoundedScrollingFrame:render()
     local children = Llama.Dictionary.join(self.props[Roact.Children], {
-        Corner = Roact.createElement("UICorner", {
+        Corner = e("UICorner", {
             CornerRadius = UDim.new(0,4);
         });
     })
@@ -23,7 +29,17 @@ function RoundedScrollingFrame:render()
         end
     end
 
-    return Roact.createElement("ScrollingFrame", props, children)
+    return f({
+        e("ScrollingFrame", props, children),
+        e(RoundedFrame, {
+            Size = self.props.Size,
+            Position = self.props.Position,
+            AnchorPoint = self.props.AnchorPoint,
+            BackgroundColor3 = self.props.BackgroundColor3,
+            BackgroundTransparency = self.props.BackgroundTransparency,
+            ZIndex = self.props.ZIndex - 1,
+        })
+    })
 end
 
 return RoundedScrollingFrame
