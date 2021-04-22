5e75a9049fd993e3abc0b4d80df73878ea6831e0
astralkingdummy <cjansah11@gmail.com>
2021-04-22 10:48:24 -0400

    cleanup

--- src/ReplicatedStorage/UI/Screens/Results/BannerCard.story.lua
+++ src/ReplicatedStorage/UI/Screens/Results/BannerCard.story.lua
@@ -1,23 +1,13 @@
 local Roact = require(game.ReplicatedStorage.Packages.Roact)
 
 local e = Roact.createElement
--- local Story = require(game.ReplicatedStorage.Shared.Utils.Story)
 
 local BannerCard = require(game.ReplicatedStorage.UI.Screens.Results.BannerCard)
 
--- function BannerCardApp:render()
---     return (BannerCard, {
---         playername = "kisperal";
---         song_key = 15;
---         rate = 100;
---         grade_image = "http://www.roblox.com/asset/?id=5702584062";
---     });
--- end
-
 return function(target)
     local app = e(BannerCard, {
-        playername = "kisperal";
-        song_key = 15;
+        playername = "GripWarrior";
+        song_key = 48;
         rate = 100;
         grade_image = "http://www.roblox.com/asset/?id=5702584062";
     });
