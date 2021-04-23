caddff393ef149a7362b5c00d97949560bafb641
thenerdie <stirmanjr@gmail.coom>
2021-04-23 00:14:52 -0400

    make sky dark.

--- src/ServerScriptService/InitServer.server.lua
+++ src/ServerScriptService/InitServer.server.lua
@@ -2,6 +2,12 @@ local Knit = require(game.ReplicatedStorage.Knit)
 
 local function game_init()
     game.Players.CharacterAutoLoads = false
+    game.Lighting.ClockTime = 0
+
+    local sky = Instance.new("Sky")
+
+    sky.CelestialBodiesShown = false
+    sky.Parent = game.Lighting
 
     Knit.AddServices(game.ServerScriptService.Services)
 
