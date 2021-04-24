11ac72a886cf8f54ba4a886d8035a62e2e42e5b3
thenerdie <stirmanjr@gmail.coom>
2021-04-24 17:14:13 -0400

    Rework options look and use Rodux

--- src/ReplicatedStorage/UI/Screens/Options/init.lua
+++ src/ReplicatedStorage/UI/Screens/Options/init.lua
@@ -1,59 +1,129 @@
 local Roact = require(game.ReplicatedStorage.Packages.Roact)
+local Rodux = require(game.ReplicatedStorage.Packages.Rodux)
+local RoactRodux = require(game.ReplicatedStorage.Packages.RoactRodux)
 local e = Roact.createElement
 local f = Roact.createFragment
+local SPUtil = require(game.ReplicatedStorage.Shared.SPUtil)
 
-local Configuration = require(game.ReplicatedStorage.Configuration)
+local Actions = require(game.ReplicatedStorage.Actions)
 
 local RoundedFrame = require(game.ReplicatedStorage.UI.Components.Base.RoundedFrame)
+local RoundedAutoScrollingFrame = require(game.ReplicatedStorage.UI.Components.Base.RoundedAutoScrollingFrame)
 local RoundedTextButton = require(game.ReplicatedStorage.UI.Components.Base.RoundedTextButton)
 
 local IntValue = require(script.IntValue)
 
 local Options = Roact.Component:extend("Options")
 
-function Options:init()
-    self:setState(Configuration.Preferences)
+Options.categoryList = { "⚙ General", "🤮 Eww" }
 
-    self.onConfigurationChanged = Configuration.Changed:Connect(function(preferences)
-        self:setState(preferences)
+function Options:init()
+    self:setState({
+        selectedCategory = 1
+    })
+end
+
+function Options:getSettingElements()
+    local elements = {}
+
+    SPUtil:switch(self.state.selectedCategory):case(1, function()
+        elements.NoteSpeed = e(IntValue, {
+            Value = self.props.options.NoteSpeed,
+            OnChanged = function(value)
+                self.props.setOption("NoteSpeed", value)
+            end,
+            FormatValue = function(value)
+                if value >= 0 then
+                    return string.format("+%d", value)
+                end
+                return string.format("%d", value)
+            end,
+            Name = "Note Speed"
+        })
+
+        elements.AudioOffset = e(IntValue, {
+            Value = self.props.options.AudioOffset,
+            OnChanged = function(value)
+                self.props.setOption("AudioOffset", value)
+            end,
+            Name = "Audio Offset",
+            FormatValue = function(value)
+                return string.format("%d ms", value)
+            end
+        })
+    end):case(2, function()
+        elements.Ew = e(IntValue, {
+            Value = self.props.options.AudioOffset,
+            OnChanged = function(value)
+                
+            end,
+            Name = "Ewwww",
+            FormatValue = function(value)
+                return string.format("%d ms", value)
+            end
+        })
     end)
+
+    return elements
 end
 
 function Options:render()
-    local options = {}
+    local options = self:getSettingElements()
 
-    options.NoteSpeed = e(IntValue, {
-        Value = self.state.NoteSpeed,
-        OnChanged = function(value)
-            Configuration:set({"NoteSpeed"}, value)
-        end,
-        Name = "Note Speed"
-    })
+    local categories = {}
 
-    options.AudioOffset = e(IntValue, {
-        Value = self.state.AudioOffset,
-        OnChanged = function(value)
-            Configuration:set({"AudioOffset"}, value)
-        end,
-        Name = "Audio Offset",
-        FormatValue = function(value)
-            return string.format("%d ms", value)
-        end
-    })
+    for i, category in ipairs(self.categoryList) do
+        local categoryButton = e(RoundedTextButton, {
+            Size = UDim2.new(1, 0, 0, 50),
+            HoldSize = UDim2.new(1, 0, 0, 53),
+            BackgroundColor3 = i == self.state.selectedCategory and Color3.fromRGB(48, 168, 184) or Color3.fromRGB(20, 20, 20),
+            Text = string.format("  - %s", category),
+            TextScaled = true,
+            TextXAlignment = Enum.TextXAlignment.Left,
+            TextColor3 = Color3.fromRGB(255, 255, 255),
+            LayoutOrder = i,
+            OnClick = function()
+                self:setState({
+                    selectedCategory = i
+                })
+            end
+        }, {
+            UITextSizeConstraint = e("UITextSizeConstraint", {
+                MaxTextSize = 20,
+                MinTextSize = 12
+            }),
+            UIAspectRatioConstraint = e("UIAspectRatioConstraint", {
+                AspectRatio = 5
+            })
+        })
+
+        table.insert(categories, categoryButton)
+    end
 
     return e(RoundedFrame, {
         Size = UDim2.fromScale(1, 1)
     }, {
-        SettingsContainer = e(RoundedFrame, {
-            Size = UDim2.fromScale(0.7, 0.8),
-            AnchorPoint = Vector2.new(0.5, 0.5),
-            Position = UDim2.fromScale(0.5, 0.5),
-            BackgroundColor3 = Color3.fromRGB(36, 36, 36)
+        SettingsContainer = e(RoundedAutoScrollingFrame, {
+            Size = UDim2.fromScale(0.3, 0.8),
+            AnchorPoint = Vector2.new(0, 0.5),
+            Position = UDim2.fromScale(0.44, 0.5),
+            BackgroundColor3 = Color3.fromRGB(36, 36, 36),
+            UIListLayoutProps = {
+                Padding = UDim.new(0, 4),
+            }
         }, {
-            UIListLayout = e("UIListLayout", {
-                HorizontalAlignment = Enum.HorizontalAlignment.Center
-            }),
             Options = f(options)
+        }),
+        SettingsCategoriesContainer = e(RoundedAutoScrollingFrame, {
+            Size = UDim2.fromScale(0.2, 0.8),
+            AnchorPoint = Vector2.new(0, 0.5),
+            Position = UDim2.fromScale(0.22, 0.5),
+            BackgroundColor3 = Color3.fromRGB(36, 36, 36),
+            UIListLayoutProps = {
+                Padding = UDim.new(0, 4),
+            }
+        }, {
+            CategoryButtons = f(categories)
         }),
         BackButton = e(RoundedTextButton, {
             Size = UDim2.fromScale(0.2, 0.05),
@@ -71,8 +141,15 @@ function Options:render()
     })
 end
 
-function Options:willUnmount()
-    self.onConfigurationChanged:Disconnect()
-end
-
-return Options
+return RoactRodux.connect(function(state)
+    return {
+        options = state.options.persistent
+    }
+end,
+function(dispatch)
+    return {
+        setOption = function(...)
+            dispatch(Actions.setPersistentOption(...))
+        end
+    }
+end)(Options)
