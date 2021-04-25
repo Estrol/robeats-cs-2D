2e54b09fb5219c4196a3242dd8d238983c365518
thenerdie <stirmanjr@gmail.coom>
2021-04-25 00:24:14 -0400

    Add dot graph component

---
+++ src/ReplicatedStorage/UI/Components/Graph/DotGraph.lua
@@ -0,0 +1,76 @@
+local Roact = require(game.ReplicatedStorage.Packages.Roact)
+local e = Roact.createElement
+local f = Roact.createFragment
+local SPList = require(game.ReplicatedStorage.Shared.SPList)
+local SPUtil = require(game.ReplicatedStorage.Shared.SPUtil)
+
+local Line = require(script.Parent.Line)
+
+local DotGraph = Roact.Component:extend("DotGraph")
+
+DotGraph.defaultProps = {
+    formatPoint = function(point)
+        return point
+    end
+}
+
+function DotGraph:render()
+    local dots = SPList:new()
+    local lines = SPList:new()
+
+    local data_points = self.props.points or {}
+
+    local max_x = 0
+    local max_y = 0
+
+    for _, data_point in ipairs(data_points) do
+        local point = self.props.formatPoint(data_point)
+        max_x = math.abs(math.max(math.abs(point.x), max_x))
+        max_y = math.abs(math.max(math.abs(point.y), max_y))
+    end
+
+    for _, data_point in ipairs(data_points) do
+        local point = self.props.formatPoint(data_point)
+
+        local _element = e("Frame", {
+            Size = UDim2.new(0,3,0,3);
+            Position = UDim2.new(point.x, 0, point.y, 0);
+            BorderSizePixel = 0;
+            BackgroundColor3 = point.color;
+        })
+        dots:push_back(_element)
+    end
+
+    local bounds = self.props.bounds
+
+    if bounds and bounds.min and bounds.max then
+        if bounds.min.y and bounds.max.y then
+            local y_int = self.props.interval and self.props.interval.y or 10
+            for y = bounds.min.y, bounds.max.y, y_int do
+                if y ~= bounds.min.y and y ~= bounds.max.y then
+                    local _element = e(Line, {
+                        value = y;
+                        position = SPUtil:inverse_lerp(bounds.min.y, bounds.max.y, y);
+                    })
+                    lines:push_back(_element)
+                end
+            end
+        end
+    end
+
+    return e("Frame", {
+        Size = self.props.Size or UDim2.new(1,0,1,0);
+        Position = self.props.Position;
+        BackgroundColor3 = self.props.BackgroundColor3 or Color3.fromRGB(35, 35, 35);
+        BorderSizePixel = self.props.BorderSizePixel or 0;
+        AnchorPoint = self.props.AnchorPoint
+    }, {
+        Corner = e("UICorner", {
+            CornerRadius = UDim.new(0,4)
+        });
+        DotElements = f(dots._table);
+        LineElements = f(lines._table)
+    })
+end
+
+return DotGraph
