74915d0e973ab92cce4d6e27129fd240625a1cee
thenerdie <stirmanjr@gmail.coom>
2021-04-25 00:24:01 -0400

    Add method to get judgement color

--- src/ReplicatedStorage/RobeatsGameCore/Enums/NoteResult.lua
+++ src/ReplicatedStorage/RobeatsGameCore/Enums/NoteResult.lua
@@ -7,6 +7,15 @@ local NoteResult = {
 	Marvelous = 5;
 }
 
+NoteResult.Colors = {
+	[NoteResult.Miss] = Color3.fromRGB(190, 30, 30),
+	[NoteResult.Bad] = Color3.fromRGB(174, 22, 194),
+	[NoteResult.Good] = Color3.fromRGB(12, 15, 151),
+	[NoteResult.Great] = Color3.fromRGB(57, 192, 16),
+	[NoteResult.Perfect] = Color3.fromRGB(235, 220, 13),
+	[NoteResult.Marvelous] = Color3.fromRGB(255, 255, 255)
+}
+
 function NoteResult:timedelta_to_result(time_to_end, _game)
 	local note_bad_max, note_good_max, note_great_max, note_perfect_max, note_marvelous_max, note_marvelous_min,  note_perfect_min, note_great_min, note_good_min,note_bad_min = _game._audio_manager:get_note_result_timing()
 	if time_to_end >= note_bad_min and time_to_end <= note_bad_max then
@@ -34,6 +43,10 @@ function NoteResult:timedelta_to_result(time_to_end, _game)
 	end	
 	
 	return false, NoteResult.Miss
+end
+
+function NoteResult:result_to_color(result)
+	return self.Colors[result]
 end
 
 return NoteResult
