f663d599f171fc2414af2e75991708c88f786fb7
astralkingdummy <cjansah11@gmail.com>
2021-05-03 16:34:05 -0400

    main menu funny

--- src/ReplicatedStorage/UI/Screens/MainMenu/init.lua
+++ src/ReplicatedStorage/UI/Screens/MainMenu/init.lua
@@ -1,6 +1,9 @@
 local Roact = require(game.ReplicatedStorage.Packages.Roact)
 
+local e = Roact.createElement
+
 local PlayerProfile = require(script.PlayerProfile)
+local MusicBox = require(script.MusicBox)
 
 local RoundedFrame = require(game.ReplicatedStorage.UI.Components.Base.RoundedFrame)
 local RoundedTextButton = require(game.ReplicatedStorage.UI.Components.Base.RoundedTextButton)
@@ -11,37 +14,41 @@ local RoundedTextLabel = require(game.ReplicatedStorage.UI.Components.Base.Round
 local MainMenuUI = Roact.Component:extend("MainMenuUI")
 
 function MainMenuUI:render()
-    return Roact.createElement(RoundedFrame, {
+    return e(RoundedFrame, {
         Size = UDim2.new(1, 0, 1, 0),
     }, {
-        Logo = Roact.createElement(RoundedImageLabel, {
+        Logo = e(RoundedImageLabel, {
             Image = "rbxassetid://6224561143";
             Size = UDim2.fromScale(0.4, 0.9);
             Position = UDim2.fromScale(0.02, 0.57);
             AnchorPoint = Vector2.new(0.05, 0.5);
             BackgroundTransparency = 1;
         }, {
-            UIAspectRatioConstraint = Roact.createElement("UIAspectRatioConstraint", {
+            UIAspectRatioConstraint = e("UIAspectRatioConstraint", {
                 AspectRatio = 1;
                 AspectType = Enum.AspectType.ScaleWithParentSize
             })
         });
-        PlayerProfile = Roact.createElement(PlayerProfile, {
+        PlayerProfile = e(PlayerProfile, {
             Size = UDim2.fromScale(0.45, 0.2)
         }),
-        ButtonContainer = Roact.createElement(RoundedFrame, {
+        -- SongBox = e(MusicBox, {
+        --     Position = UDim2.fromScale(0.25, 0.02),
+        --     SongKey = 1;
+        -- }),
+        ButtonContainer = e(RoundedFrame, {
             Size = UDim2.fromScale(0.25, 0.6);
             Position = UDim2.fromScale(0.02,0.95);
             AnchorPoint = Vector2.new(0, 1);
             BackgroundTransparency = 1;
         },{
-            UIListLayout = Roact.createElement("UIListLayout", {
+            UIListLayout = e("UIListLayout", {
                 Padding = UDim.new(0.015,0);
                 SortOrder = Enum.SortOrder.LayoutOrder;
                 VerticalAlignment = Enum.VerticalAlignment.Bottom;
             });
 
-            PlayButton = Roact.createElement(RoundedTextButton, {
+            PlayButton = e(RoundedTextButton, {
                 TextXAlignment = Enum.TextXAlignment.Left;
                 BackgroundColor3 = Color3.fromRGB(22, 22, 22);
                 BorderMode = Enum.BorderMode.Inset,
@@ -56,12 +63,34 @@ function MainMenuUI:render()
                     self.props.history:push("/select")
                 end
             }, {
-                UITextSizeConstraint = Roact.createElement("UITextSizeConstraint", {
+                UITextSizeConstraint = e("UITextSizeConstraint", {
                     MinTextSize = 8;
                     MaxTextSize = 13;
                 })
             });
-            OptionsButton = Roact.createElement(RoundedTextButton, {
+
+            -- MultiButton = e(RoundedTextButton, {
+            --     TextXAlignment = Enum.TextXAlignment.Left;
+            --     BackgroundColor3 = Color3.fromRGB(70, 69, 69);
+            --     BorderMode = Enum.BorderMode.Inset,
+            --     BorderSizePixel = 0,
+            --     Size = UDim2.fromScale(1, 0.125),
+            --     Text = "  Multiplayer (Coming Soon)";
+            --     TextScaled = true;
+            --     TextColor3 = Color3.fromRGB(255, 255, 255);
+            --     LayoutOrder = 1;
+            --     HoldSize = UDim2.fromScale(0.95, 0.125),
+            --     OnClick = function()
+            --         self.props.history:push("/select")
+            --     end
+            -- }, {
+            --     UITextSizeConstraint = e("UITextSizeConstraint", {
+            --         MinTextSize = 8;
+            --         MaxTextSize = 13;
+            --     })
+            -- });
+
+            OptionsButton = e(RoundedTextButton, {
                 TextXAlignment = Enum.TextXAlignment.Left;
                 BackgroundColor3 = Color3.fromRGB(22, 22, 22);
                 BorderMode = Enum.BorderMode.Inset,
@@ -76,12 +105,12 @@ function MainMenuUI:render()
                     self.props.history:push("/options")
                 end
             }, {
-                UITextSizeConstraint = Roact.createElement("UITextSizeConstraint", {
+                UITextSizeConstraint = e("UITextSizeConstraint", {
                     MinTextSize = 8;
                     MaxTextSize = 13;
                 })
             });
-            GlobalLeaderboardButton = Roact.createElement(RoundedTextButton, {
+            GlobalLeaderboardButton = e(RoundedTextButton, {
                 TextXAlignment = Enum.TextXAlignment.Left;
                 BackgroundColor3 = Color3.fromRGB(22, 22, 22);
                 BorderMode = Enum.BorderMode.Inset,
@@ -96,14 +125,14 @@ function MainMenuUI:render()
                     self.props.history:push("/rankings")
                 end
             }, {
-                UITextSizeConstraint = Roact.createElement("UITextSizeConstraint", {
+                UITextSizeConstraint = e("UITextSizeConstraint", {
                     MinTextSize = 8;
                     MaxTextSize = 13;
                 })
             });
         });
 
-        Title = Roact.createElement("TextLabel", {
+        Title = e("TextLabel", {
             BackgroundTransparency = 1;
             BackgroundColor3 = Color3.fromRGB(255, 255, 255);
             Text = "RoBeats Community Server";
