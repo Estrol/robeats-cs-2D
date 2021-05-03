7d14153a91427aaeb84ff2a3659772e610dca957
thenerdie <stirmanjr@gmail.coom>
2021-05-03 13:59:54 -0400

    Implement profile related APIs

--- src/ServerScriptService/Services/ScoreService.lua
+++ src/ServerScriptService/Services/ScoreService.lua
@@ -8,6 +8,7 @@ local DebugOut = require(game.ReplicatedStorage.Shared.DebugOut)
 local ParseServer = require(game.ReplicatedStorage.Packages.ParseServer)
 local Server
 local Scores
+local Global
 
 local baseUrl
 
@@ -27,18 +28,108 @@ function ScoreService:KnitInit()
         baseUrl = game:GetService("ServerScriptService").URLs.Release.Value
     end
 
-    DebugOut:puts("Base API URL: %s", baseUrl)
-end
-
-function ScoreService:KnitStart()
     local SecretService = Knit.GetService("SecretService")
-
+    
     Scores = Server.Objects.class("Plays")
+    Global = Server.Objects.class("Global")
     Server:setAppId(SecretService:GetSecret("ParseAppId")):setBaseUrl(baseUrl)
+
+    DebugOut:puts("Base API URL: %s", baseUrl)
 end
 
 function ScoreService:_GetGraphKey(userId, songMD5Hash)
     return string.format("Graph(%s%s)", userId, songMD5Hash)
+end
+
+function ScoreService:GetPlayerScores(userId, limit)
+    local succeeded, documents = Scores
+        :query()
+        :where({
+            UserId = userId
+        })
+        :order("-Rating")
+        :limit(limit)
+        :execute()
+        :await()
+
+    if succeeded then
+        return documents, succeeded
+    else
+        warn(documents)
+        return {}, succeeded
+    end
+end
+
+function ScoreService:CalculateRating(userId)
+    local scores = self:GetPlayerScores(userId)
+
+    local rating = 0;
+    local maxNumOfScores = math.min(#scores, 25);
+
+    for i = 1, maxNumOfScores do
+        if i > 10 then
+            rating = rating + scores[i].Rating * 1.5
+        else
+            rating = rating + scores[i].Rating;
+        end
+    end
+
+    return math.floor((100 * rating) / 30) / 100
+end
+
+function ScoreService:CalculateAverageAccuracy(userId)
+    local scores = self:GetPlayerScores(userId)
+
+    local accuracy = 0
+
+    for _, score in ipairs(scores) do
+        accuracy += score.Accuracy
+    end
+
+    return accuracy / #scores
+end
+
+function ScoreService:RefreshProfile(userId, playerName)
+    local succeeded, slots = Global
+            :query()
+            :where({
+                UserId = userId
+            })
+            :execute()
+            :await()
+
+    if succeeded then
+        local slot = slots[1]
+
+        if slot then
+            Global
+                :update(slot.objectId, {
+                    TotalMapsPlayed = {
+                        __op = "Increment",
+                        amount = 1
+                    },
+                    Rating = ScoreService:CalculateRating(userId),
+                    Accuracy = ScoreService:CalculateAverageAccuracy(userId),
+                    PlayerName = playerName,
+                    UserId = userId
+                })
+                :andThen(function(document)
+                    DebugOut:puts("Global leaderboard slot successfully updated!")
+                end)
+        else
+            Global
+                :create({
+                    TotalMapsPlayed = 1,
+                    Rating = ScoreService:CalculateRating(userId),
+                    Accuracy = ScoreService:CalculateAverageAccuracy(userId),
+                    PlayerName = playerName,
+                    UserId = userId
+                })
+                :andThen(function(document)
+                    DebugOut:puts("Global leaderboard slot successfully created!")
+                end)
+        end
+    end
 end
 
 function ScoreService.Client:SubmitScore(player, songMD5Hash, rating, score, marvelouses, perfects, greats, goods, bads, misses, accuracy, maxChain, mean, rate)
@@ -73,6 +164,7 @@ function ScoreService.Client:SubmitScore(player, songMD5Hash, rating, score, mar
                 SongMD5Hash = songMD5Hash
             })
             :await()
+            ScoreService:RefreshProfile(player.UserId, player.DisplayName)
             return succeeded
         end
 
@@ -98,11 +190,10 @@ function ScoreService.Client:SubmitScore(player, songMD5Hash, rating, score, mar
                 Mean = mean,
                 Accuracy = accuracy,
                 Rate = rate,
-            })
-            :andThen(function(document)
-                print(document)
-            end)
+            }):await()
         end
+
+        ScoreService:RefreshProfile(player.UserId, player.DisplayName)
     else
         return false
     end
@@ -138,6 +229,67 @@ function ScoreService.Client:GetScores(_, songMD5Hash)
         warn(documents)
         return {}, succeeded
     end
+end
+
+function ScoreService.Client:GetRank(player)
+    local succeeded, ranks = Global
+        :query({})
+        :order("-Rating")
+        :execute()
+        :await()
+
+    if not succeeded then
+        warn(ranks)
+        return
+    end
+    
+    for rank = 1, #ranks do
+        local profile = ranks[rank]
+
+        if profile.UserId == player.UserId then
+            return rank
+        end
+    end
+
+    return -1
+end
+
+function ScoreService.Client:GetProfile(player)
+    local succeeded, profiles = Global
+        :query({
+            UserId = player.UserId
+        })
+        :order("-Rating")
+        :execute()
+        :await()
+
+    if not succeeded then
+        warn(profiles)
+        return
+    end
+
+    local profile = profiles[1]
+
+    if profile then
+        return profile
+    end
+
+    return {}
+end
+
+function ScoreService.Client:GetGlobalLeaderboard()
+    local succeeded, ranks = Global
+        :query({})
+        :order("-Rating")
+        :execute()
+        :await()
+
+    if succeeded then
+        return ranks
+    end
+
+    warn(ranks)
+    return {}
 end
 
 return ScoreService
