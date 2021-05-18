44a249b0a8bc88036e38c8cdc6befd67d7fcd962
thenerdie <stirmanjr@gmail.coom>
2021-05-18 12:41:32 -0400

    Implement rate limiting

---
+++ src/ServerScriptService/Services/RateLimitService.lua
@@ -0,0 +1,46 @@
+local Knit = require(game:GetService("ReplicatedStorage").Knit)
+
+local SPUtil = require(game.ReplicatedStorage.Shared.SPUtil)
+
+local RateLimitService = Knit.CreateService {
+    Name = "RateLimitService";
+    Client = {};
+}
+
+RateLimitService.Requests = {}
+
+function RateLimitService:CanProcessRequestWithRateLimit(player, eventName, requestsPerSecond)
+    local userRequests = SPUtil:filter(self.Requests, function(request)
+        return (request.player == player) and (request.eventName == eventName)
+    end)
+
+    local keysToRemove = {}
+
+    for i, v in ipairs(userRequests) do
+        if tick() - v.time >= 1 then
+            table.insert(keysToRemove, {
+                globalId = v._id,
+                localId = i
+            })
+        end
+    end
+
+    for _, v in ipairs(keysToRemove) do
+        table.remove(self.Requests, v.globalId)
+        table.remove(userRequests, v.localId)
+    end
+
+    if #userRequests > requestsPerSecond then
+        return false
+    end
+
+    table.insert(self.Requests, {
+        player = player,
+        eventName = eventName,
+        time = tick()
+    })
+
+    return true
+end
+
+return RateLimitService
