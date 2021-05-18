4c213f66dc1b5131a140f786332d5d1e3df1100a
thenerdie <stirmanjr@gmail.coom>
2021-05-17 22:14:38 -0400

    Fix main menu story

--- src/ReplicatedStorage/UI/Screens/MainMenu.story.lua
+++ src/ReplicatedStorage/UI/Screens/MainMenu.story.lua
@@ -1,11 +1,38 @@
 local Roact = require(game.ReplicatedStorage.Packages.Roact)
+local RoactRodux = require(game.ReplicatedStorage.Packages.RoactRodux)
 local e = Roact.createElement
+local State = require(game.ReplicatedStorage.State)
+
+local Actions = require(game.ReplicatedStorage.Actions)
+
+local DIContext = require(game.ReplicatedStorage.Contexts.DIContext)
+
+local Fitumi = require(game.ReplicatedStorage.Packages.Fitumi)
+local a = Fitumi.a
 
 local MainMenu = require(script.Parent.MainMenu)
 
 return function(target)
+    State.Store:dispatch(Actions.setAdmin(true))
 
-    local app = e(MainMenu)
+    local storeProvider = Roact.createElement(RoactRodux.StoreProvider, {
+        store = State.Store
+    }, {
+        App = Roact.createElement(MainMenu)
+    })
+
+    local fakePreviewController = a.fake()
+
+    a.callTo(fakePreviewController.GetSoundInstance, fakePreviewController)
+        :returns(Instance.new("Sound"))
+
+    local app = Roact.createElement(DIContext.Provider, {
+        value = {
+            PreviewController = fakePreviewController
+        }
+    }, {
+        StoreProvider = storeProvider
+    })
 
     local handle = Roact.mount(app, target)
 
