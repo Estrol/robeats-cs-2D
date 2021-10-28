35f4162bc25d6f138d8eb82501a52ee235b12029
astralkingdummy <cjansah11@gmail.com>
2021-10-27 20:47:27 -0400

    chat tag service thing

---
+++ src/ServerScriptService/Services/ChatTagService.lua
@@ -0,0 +1,35 @@
+local Knit = require(game.ReplicatedStorage.Knit)
+
+local PermissionsService
+local ChatService
+
+local ChatTagService = Knit.CreateService {
+    Name = "ChatTagService";
+    Client = {}
+}
+
+ChatTagService.TagData = {
+    Staff = {TagText = "STAFF", TagColor = Color3.fromRGB(248, 113, 113)},
+}
+
+function ChatTagService:KnitStart()
+    PermissionsService = Knit.GetService("PermissionsService")
+    ChatService = require(game.ServerScriptService:WaitForChild("ChatServiceRunner"):WaitForChild("ChatService"))
+
+    ChatService.SpeakerAdded:Connect(self.HandleSpeakerAdded)
+
+    for _, speaker in ipairs(ChatService:GetSpeakerList()) do
+        self.HandleSpeakerAdded(speaker)
+    end
+end
+
+function ChatTagService.HandleSpeakerAdded(speakerName)
+    local speaker = ChatService:GetSpeaker(speakerName)
+    local player = speaker:GetPlayer()
+
+    if player and PermissionsService:HasModPermissions(player) then
+        speaker:SetExtraData("Tags", { ChatTagService.TagData.Staff })
+    end
+end
+
+return ChatTagService
