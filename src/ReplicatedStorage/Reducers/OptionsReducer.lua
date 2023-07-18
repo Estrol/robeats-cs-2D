edd8e43d45fb03d86bc4c133df3e3fe7f1dab40b
astralkingdummy <cjansah11@gmail.com>
2023-07-17 23:34:25 -0400

    add defaults to presets in options reducer

--- src/ReplicatedStorage/Reducers/OptionsReducer.lua
+++ src/ReplicatedStorage/Reducers/OptionsReducer.lua
@@ -55,6 +55,12 @@ local defaultState = {
         --Custom judgements
         UseCustomJudgements = false;
 
+        CustomMarvelousPreset = 22;
+        CustomPerfectPreset = 45;
+        CustomGreatPreset = 85;
+        CustomGoodPreset = 112;
+        CustomBadPreset = 136;
+
         --Mobile Settings
         DividersEnabled = true,
 
